7845c922eea48fbfd5d50b50c8ff470dbd1aae5f
nosferatu <51732890+nosferatu1cmyk@users.noreply.github.com>
2022-01-14 10:29:40 -0300

    Add files via upload

---
+++ src/js/data/2021-05-09.js
@@ -0,0 +1,303 @@
+dataSetVersion = "2021-05-09"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  
+  
+
+];
+
+dataSet[dataSetVersion].characterData = [
+
+  {
+    name: "박보은 PARK BOEUN",
+    img: "xjZWUcA.jpg",
+    opts: {
+      series: ["book", "EoSD", "IaMP", "StB", "soku"],
+      stage: ["st3"]
+    }
+  },
+  {
+    name: "윤승주 YOON SEUNGJU",
+    img: "PnfUpB4.jpg",
+    opts: {
+      series: ["book", "EoSD"],
+      stage: ["st4"],
+      nameless: true
+    }
+  },
+  {
+    name: "정시우 JUNG SIWOO",
+    img: "RO8lgjV.jpg",
+    opts: {
+      series: ["book", "EoSD", "IaMP", "StB", "SWR", "soku", "HM"],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "보미세라 BOHME SARA",
+    img: "Ur2Y2xy.jpg",
+    opts: {
+      series: [ "book", "EoSD", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "soku", "HM", "DDC", "ISC", "UM" ],
+      stage: ["st5", "st6"]
+    }
+  },
+  {
+    name: "정유주 JUNG YUJU",
+    img: "D1BnUrX.jpg",
+    opts: {
+      series: ["book", "EoSD", "IaMP", "IN", "StB", "SWR", "soku", "HM", "ISC", "VD"],
+      stage: ["st6"]
+    }
+  },
+  {
+    name: "김수빈 KIM SUBEEN",
+    img: "Ls8ZyfF.jpg",
+    opts: {
+      series: ["book", "EoSD", "StB", "VD"],
+      stage: ["ex"]
+    }
+  },
+  {
+    name: "김선유 KIM SEONYOU",
+    img: "wzc0FAq.jpg",
+    opts: {
+      series: ["book", "PCB", "StB", "HM"],
+      stage: ["st1"]
+    }
+  },
+  {
+    name: "오유진 OH YOOJIN",
+    img: "bnBamHk.jpg",
+    opts: {
+      series: ["book", "PCB", "IaMP", "StB", "SWR", "soku", "HM"],
+      stage: ["st2", "ex"]
+    }
+  },
+  {
+    name: "최수빈 CHOI SOOBIN",
+    img: "5IAj12H.jpg",
+    opts: {
+      series: ["book", "PCB", "IaMP", "IN", "StB", "SWR", "soku", "HM"],
+      stage: ["st3"]
+    }
+  },
+  {
+    name: "성민채 SUNG MINCHAE",
+    img: "rGO0xVk.jpg",
+    opts: {
+      series: ["book", "PCB", "PoFV", "HM", "HSiFS"],
+      stage: ["st4", "ex"]
+    }
+  },
+  {
+    name: "이지원 LEE JIWON",
+    img: "EsHBtHC.jpg",
+    opts: {
+      series: ["book", "PCB", "PoFV", "HM", "AoCF"],
+      stage: ["st4"]
+    }
+  },
+  {
+    name: "원지민 WON JIMIN",
+    img: "b030g4b.jpg",
+    opts: {
+      series: ["book", "PCB", "PoFV", "HM", "AoCF"],
+      stage: ["st4"]
+    }
+  },
+  {
+    name: "미나미 MINAMI",
+    img: "ic7mdV4.jpg",
+    opts: {
+      series: ["book", "PCB", "PoFV", "HM", "AoCF"],
+      stage: ["st4"]
+    }
+  },
+  {
+    name: "김리원 KIM RIWON",
+    img: "d3Tvev9.jpg",
+    opts: {
+      series: [ "book", "PCB", "IaMP", "IN", "PoFV", "StB", "SWR", "soku", "TD", "HM", "ISC", "WBaWC" ],
+      stage: ["st5", "st6"]
+    }
+  },
+  {
+    name: "최사랑 CHOI SARANG",
+    img: "ZD9cbhx.jpg",
+    opts: {
+      series: [ "book", "PCB", "IaMP", "IN", "StB", "SWR", "soku", "TD", "HM", "ISC", "VD" ],
+      stage: ["st1", "st6"]
+    }
+  },
+  {
+    name: "이영채 LEE YOUNGCHAE",
+    img: "3aiEhJa.jpg",
+    opts: {
+      series: ["book", "PCB", "IaMP", "IN", "StB", "SWR", "soku", "HM", "VD"],
+      stage: ["ex"]
+    }
+  },
+  {
+    name: "조수이 CHO SUI",
+    img: "GuSlYo9.jpg",
+    opts: {
+      series: [ "book", "PCB", "IaMP", "IN", "StB", "SWR", "soku", "HM", "ISC", "AoCF", "VD" ],
+      stage: ["ex"]
+    }
+  },
+  {
+    name: "박효원 PARK HYOWON",
+    img: "nJDa2jS.jpg",
+    opts: {
+      series: ["book", "IaMP", "StB", "SWR", "soku", "DS", "HM", "ISC", "VD"],
+      stage: ["st6"]
+    }
+  },
+  {
+    name: "타케이 카리나 TAKEI KARINA",
+    img: "tMQDJV6.jpg",
+    opts: {
+      series: ["book", "IN", "StB", "HM"],
+      stage: ["st1"]
+    }
+  },
+  {
+    name: "최윤정 CHOI YOONJUNG",
+    img: "ewwvQ5K.jpg",
+    opts: {
+      series: ["book", "IN", "PoFV", "StB", "HM"],
+      stage: ["st2"]
+    }
+  },
+  {
+    name: "김윤서 KIM YUNSEO",
+    img: "NNBXO9x.jpg",
+    opts: {
+      series: ["book", "IN", "StB", "HM", "ISC"],
+      stage: ["st3", "ex"]
+    }
+  },
+  {
+    name: "김현희 KIM HYUNHEE",
+    img: "WQGPbxB.jpg",
+    opts: {
+      series: ["book", "IN", "PoFV", "StB", "HM"],
+      stage: ["st5"]
+    }
+  },
+  {
+    name: "오지은 OH JIEUN",
+    img: "swbjTIa.jpg",
+    opts: {
+      series: ["book", "IN", "PoFV", "StB", "soku", "HM", "LoLK", "AoCF"],
+      stage: ["st5"]
+    }
+  },
+  {
+    name: "유재현 RYOU JAYHYUN",
+    img: "XfK1lhn.jpg",
+    opts: {
+      series: ["book", "IN", "StB", "HM", "VD"],
+      stage: ["st6"]
+    }
+  },
+  {
+    name: "이태림 LEE TAERIM",
+    img: "qS0UzzE.jpg",
+    opts: {
+      series: ["book", "IN", "StB", "HM", "VD"],
+      stage: ["st6"]
+    }
+  },
+  {
+    name: "김수혜 KIM SUHYE",
+    img: "nlkSIF7.jpg",
+    opts: {
+      series: ["book", "IN", "StB", "HM", "ISC", "ULiL", "AoCF", "VD"],
+      stage: ["ex"]
+    }
+  },
+  {
+    name: "이지우 LEE JIWOO",
+    img: "xNlMLZh.jpg",
+    opts: {
+      series: [ "book", "PoFV", "StB", "SWR", "MoF", "soku", "HM", "ISC", "HSiFS" ],
+      stage: ["st4"]
+    }
+  },
+  {
+    name: "윤채원 YOON CHAEWON",
+    img: "66ituzp.jpg",
+    opts: {
+      series: ["book", "PoFV", "StB", "HM"],
+      stage: ["st4"]
+    }
+  },
+  {
+    name: "이미희LEE MIHEE",
+    img: "xKOVFyR.jpg",
+    opts: {
+      series: ["book", "PoFV", "StB", "HM"],
+      stage: ["st5"]
+    }
+  },
+  {
+    name: "김유연 KIM YOOYEON",
+    img: "Pqh6XAF.jpg",
+    opts: {
+      series: ["book", "PoFV", "StB", "SWR", "soku", "HM"],
+      stage: ["st5"]
+    }
+  },
+  {
+    name: "김인혜 KIM INHYE",
+    img: "alMuo4Z.jpg",
+    opts: {
+      series: ["book", "PoFV", "StB", "HM", "VD"],
+      stage: ["st6"]
+    }
+  },
+  {
+    name: "명형서 MYUNG HYUNGSEO",
+    img: "XSHHeN4.jpg",
+    opts: {
+      series: ["MoF", "DS", "HM"],
+      stage: ["st1"]
+    }
+  },
+  {
+    name: "홍혜주 HONG HYEJU",
+    img: "5iWHYRB.jpg",
+    opts: {
+      series: ["MoF", "DS", "HM"],
+      stage: ["st1"]
+    }
+  },
+  {
+    name: "김하리 KIM HARI",
+    img: "eTR8eaL.jpg",
+    opts: {
+      series: ["MoF", "DS", "HM"],
+      stage: ["st2"]
+    }
+  },
+  {
+    name: "이유민 LEE YUMIN",
+    img: "539SaxC.jpg",
+    opts: {
+      series: ["MoF", "DS", "HM", "ISC", "ULiL", "AoCF"],
+      stage: ["st3"]
+    }
+  },
+  {
+    name: "송예림 SONG YERIM",
+    img: "68f1jyn.jpg",
+    opts: {
+      series: ["MoF", "DS", "ISC"],
+      stage: ["st4"]
+    }
+  },
+  
+  
+];
